2b1f2d3cf715f691706b2a6b08adef671763d3dd
RT Sullivan <atleast24.7@list.ru>
2017-08-18 15:58:35 +0800

    HOT REPLACEMENT MODE

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   entry: './src/app.js',
@@ -11,13 +12,17 @@ module.exports = {
   module: {
     rules: [
       {test: /\.scss$/, 
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader'],
-          publicPath: './dist'
-        })
+        use: ['style-loader','css-loader', 'sass-loader'],
       }
     ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    stats: 'errors-only',
+    open: true,
+    hot: true
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -29,6 +34,9 @@ module.exports = {
     }),
     new ExtractTextPlugin({
       filename: 'style.css',
-    })
+      disable: true
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
   ]
 };
